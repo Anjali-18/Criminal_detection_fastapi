6f246f07b9b7d50a4aaf32cd469e4185e06e49df
Anjali <anjali18jaiswal@gmali.com>
2021-04-11 15:51:38 +0530

    message

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn app:cfd --host=0.0.0.0 --port=${PORT:-5000}
---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, File, UploadFile, Response
+from pydantic import BaseModel
+import face_rec as frc
+import numpy as np
+import cv2
+from starlette.responses import FileResponse
+
+cfd = FastAPI()
+
+@cfd.get("/")
+def home():
+    return "Welcome to criminal face detection API"
+
+@cfd.get("/impro")
+def impro():
+    return ''
+
+@cfd.post("/impro")
+async def upload_image(file: UploadFile=File(...)):
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    matched_image = frc.classify_face(img)
+    #print("hii")
+    img = cv2.imread('matched_image.png')
+    # return frc.classify_face(img)
+    return FileResponse("matched_image.png", media_type='application/octet-stream',filename="matched_image.png")
