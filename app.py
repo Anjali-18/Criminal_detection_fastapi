9801e3680a8ba9d6fbafd550ddaa862b43969aa3
Anjali-18 <60153249+Anjali-18@users.noreply.github.com>
2021-04-29 13:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,24 @@ import face_rec as frc
 import numpy as np
 import cv2
 from starlette.responses import FileResponse
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image 
 
 cfd = FastAPI()
 
-@cfd.get("/")
-def home():
-    return "Welcome to criminal face detection API"
+origins = ["*"]
+
+cfd.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# @cfd.get("/")
+# def home():
+#     return "Welcome to criminal face detection API"
 
 @cfd.get("/impro")
 def impro():
@@ -24,18 +36,12 @@ async def upload_image(file: UploadFile=File(...)):
     matched_image = frc.classify_face(img)
     #print("hii")
     img = cv2.imread('matched_image.png')
+    print(matched_image)
+
     # return frc.classify_face(img)
+    # return Response("matched_image.png", media_type='application/octet-stream',filename="matched_image.png",)
     return FileResponse("matched_image.png", media_type='application/octet-stream',filename="matched_image.png")
 
-
-# @cfd.post("/improtest")
-# async def upload_image(file: File):
-#     contents = await file.read()
-#     nparr = np.fromstring(contents, np.uint8)
-#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#     matched_image = frc.classify_face(img)
-#     output = Response(content=matched_image, media_type="image/png")
-#     return output
 
 if __name__ == "__app__":
     uvicorn.run(app, debug=True, port=8000)
